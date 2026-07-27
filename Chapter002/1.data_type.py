9d745305a528ef76bf581eaf39c02635bdbbd8b9
DarrylHuang <darrylhuang@outlook.com>
2026-07-27 10:44:51 +0800

    data type base knowledge

---
+++ Chapter002/1.data_type.py
@@ -0,0 +1,38 @@
+# int
+print(100)
+print(-100)
+
+# float
+print(3.14)
+
+# str
+print("Hello Python")
+
+# bool
+print(True)
+print(False)
+
+# NoneType
+print(None)
+
+print(type("Hello Python"))
+print(isinstance("Hello Python", bool))
+
+# the bool value is not only bool type but int type
+print(isinstance(True, int))
+print(isinstance(False, bool))
+
+# str definition ways
+single_quotation = 'Hello\n'
+double_quotation = "Python" # recommended
+multiple_rows = """
+    Hello
+    Python
+    !
+"""
+print(single_quotation)
+print(double_quotation)
+print("Hello\nWorld")
+
+print(single_quotation, double_quotation, multiple_rows)
+print("Hello\tPython")
---
+++ Chapter002/2.variable.py
@@ -0,0 +1,25 @@
+num = 3.14
+print(num)
+
+num = num + num
+print(num)
+
+num = 2.71828
+print(num)
+
+num = "python is a dynamic data type language. A variable can change its data type multiple times during the assignment process without the need for advance declaration as in strong data types. However, in actual development, unless there are special situation, it is still recommended that the value of a variable store data of one type."
+print(num)
+
+# multiple variables definition
+a, b = 4, 9
+print(a, b)
+
+# Rules
+# No.1 It can only contain lowercase characters, uppercase characters and underscores
+# No.2 do not start with number
+# No.3 do not use the keywords cleanly, such as True, False, None, and, or, if, else, elif, for, while ...
+# No.4 the name is case sensitive
+
+# standard
+# No.1 it is recommended that the variable name is suppose to lowercase
+# No.2 it is recommended that multipart of words should be snake naming which is linked with underscores
