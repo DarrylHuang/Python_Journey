1a622f840376bb8ae393ea15eac29b5de7157164
Darryl <darrylhuang@outlook.com>
2026-08-07 09:42:26 +0800

    bingo

--- Chapter2/5.calculation.py
+++ Chapter2/5.calculation.py
@@ -26,36 +26,39 @@ print(num > 4 and num < 10)
 # simplified
 print("simplified: ", 4 < num < 10)
 
-# priority high to low, same level calc operator processing is left-to-right:
-# level1: ()
-# level2: **
-# level3: *, /, // %
-# level4: +, -
-# level6: <<, >>
-# level7: &
-# level8: ^
-# level9: |
-# level10: ==, !=, >, <, >=, <=, is, is not, in, not in
-# level11: not
-# level12: and
-# level13: or
-# level14:
-#     +--------------------------------+-----------------+-----------------+------------------------------------------+
-#     | Category                       | Normal Form     | Augmented Form  | Equivalent Meaning                       |
-#     +--------------------------------+-----------------+-----------------+------------------------------------------+
-#     | Basic Assignment               | a = b           | N/A             | Direct assignment                        |
-#     | Arithmetic Addition            | a = a + b       | a += b          | Addition                                 |
-#     | Arithmetic Subtraction         | a = a - b       | a -= b          | Subtraction                              |
-#     | Arithmetic Multiplication      | a = a * b       | a *= b          | Multiplication                           |
-#     | Arithmetic Division            | a = a / b       | a /= b          | Division (float)                         |
-#     | Arithmetic Floor Div           | a = a // b      | a //= b         | Floor division                           |
-#     | Arithmetic Modulo              | a = a % b       | a %= b          | Modulo (remainder)                       |
-#     | Arithmetic Exponentiation      | a = a ** b      | a **= b         | Exponentiation                           |
-#     | Shift Left                     | a = a << b      | a <<= b         | Left shift                               |
-#     | Shift Right                    | a = a >> b      | a >>= b         | Right shift                              |
-#     | Bitwise AND                    | a = a & b       | a &= b          | Bitwise AND                              |
-#     | Bitwise OR                     | a = a | b       | a |= b          | Bitwise OR                               |
-#     | Bitwise XOR                    | a = a ^ b       | a ^= b          | Bitwise XOR                              |
-#     | Special (Walrus)               | a = expression  | :=              | Assignment expr. (Python 3.8+)           |
-#     +--------------------------------+-----------------+-----------------+------------------------------------------+
+"""
+priority high to low, same level calc operator processing is left-to-right:
+level1: ()
+level2: **
+level3: *, /, // %
+level4: +, -
+level6: <<, >>
+level7: &
+level8: ^
+level9: |
+level10: ==, !=, >, <, >=, <=, is, is not, in, not in
+level11: not
+level12: and
+level13: or
+level14:
+    +--------------------------------+-----------------+-----------------+------------------------------------------+
+    | Category                       | Normal Form     | Augmented Form  | Equivalent Meaning                       |
+    +--------------------------------+-----------------+-----------------+------------------------------------------+
+    | Basic Assignment               | a = b           | N/A             | Direct assignment                        |
+    | Arithmetic Addition            | a = a + b       | a += b          | Addition                                 |
+    | Arithmetic Subtraction         | a = a - b       | a -= b          | Subtraction                              |
+    | Arithmetic Multiplication      | a = a * b       | a *= b          | Multiplication                           |
+    | Arithmetic Division            | a = a / b       | a /= b          | Division (float)                         |
+    | Arithmetic Floor Div           | a = a // b      | a //= b         | Floor division                           |
+    | Arithmetic Modulo              | a = a % b       | a %= b          | Modulo (remainder)                       |
+    | Arithmetic Exponentiation      | a = a ** b      | a **= b         | Exponentiation                           |
+    | Shift Left                     | a = a << b      | a <<= b         | Left shift                               |
+    | Shift Right                    | a = a >> b      | a >>= b         | Right shift                              |
+    | Bitwise AND                    | a = a & b       | a &= b          | Bitwise AND                              |
+    | Bitwise OR                     | a = a | b       | a |= b          | Bitwise OR                               |
+    | Bitwise XOR                    | a = a ^ b       | a ^= b          | Bitwise XOR                              |
+    | Special (Walrus)               | a = expression  | :=              | Assignment expr. (Python 3.8+)           |
+    +--------------------------------+-----------------+-----------------+------------------------------------------+
+"""
+
 
